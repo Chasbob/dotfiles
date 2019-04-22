c87f32423bdf6ef5bc3a8c02440722abdec3bcc1
Charlie de Freitas <ypph279@gmail.com>
2019-04-22 10:05:15 +0100

    Remove config alias from install script.

--- docs/install.sh
+++ docs/install.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-
 git clone --bare https://github.com/Chasbob/dotfiles.git $HOME/.cfg
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-mkdir -p .config-backup && \
-config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
-xargs -I{} mv {} .config-backup/{}
-config checkout
-config config --local status.showUntrackedFiles no
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
