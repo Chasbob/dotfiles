e7a30ad42215bc5bd975d67435014931dfc62d04
Charles de Freitas <charles.de.freitas00@gmail.com>
2019-05-15 17:43:22 +0100

    Move aliases to zshrc, remove doc directoy with install script

--- docs/install.sh
+++
@@ -1,3 +0,0 @@
-git clone --bare https://github.com/Chasbob/dotfiles.git $HOME/.cfg
-git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
-git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
