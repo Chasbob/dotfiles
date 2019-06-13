b91b829488739c042f4233add627baa9aa797bb8
Charles de Freitas <charles.de.freitas00@gmail.com>
2019-06-13 11:40:31 +0100

    Re-add install script.

---
+++ docs/install.sh
@@ -0,0 +1,3 @@
+git clone --bare https://github.com/Chasbob/dotfiles.git $HOME/.cfg
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
