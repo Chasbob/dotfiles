e5be6ee41606ed6cdebc05641b9b4f517e29b53f
Charles de Freitas <charles@defreitas.io>
2019-07-11 13:31:21 +0200

    Fix install not checking out.

--- docs/install.sh
+++ docs/install.sh
@@ -1,4 +1,5 @@
 git clone --bare https://github.com/Chasbob/dotfiles.git $HOME/.cfg
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME submodule init
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME submodule update
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
