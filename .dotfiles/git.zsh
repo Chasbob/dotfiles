ee50039ffc4bc76801dde9f2cd58fe406d8d7c0a
Charles de Freitas <charles@defreitas.io>
2019-11-01 19:47:36 +0000

    Add function for updating author in git history

--- .dotfiles/git.zsh
+++ .dotfiles/git.zsh
@@ -4,3 +4,20 @@ alias gc="git commit"
 alias gs="git status"
 alias ga="git add"
 alias ggo="git remote get-url origin"
+
+function fix-author(){
+        git filter-branch -f --env-filter '
+
+        OLD_EMAIL="charles.defreitas@progressiveaccess.com"
+        CORRECT_EMAIL="charles@defreitas.io"
+
+        if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+        then
+            export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+        fi
+        if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+        then
+            export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+        fi
+        ' --tag-name-filter cat -- --branches --tags
+}
