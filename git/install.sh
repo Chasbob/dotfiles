62bdff2e0d25d3c05a0979e86ab3ed3e8916e2c0
Charles de Freitas <charles@defreitas.io>
2020-06-26 15:42:51 +0100

    Clone zplug during zsh install

--- git/install.sh
+++ git/install.sh
@@ -5,5 +5,5 @@ SOURCE=$1
 echo "Setup git..."
 test -e "$SOURCE"/git/.gitconfig && mv "$HOME"/.gitconfig{,.bak}
 test -L "$SOURCE"/git/.gitconfig && mv "$HOME"/.gitconfig{,.bak}
-ln -s git/.gitconfig "$HOME"/.gitconfig
+ln -s "$SOURCE"/git/.gitconfig "$HOME"/.gitconfig
 
