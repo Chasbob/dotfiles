7c34da84783040d723c5830dbcb56366761fc0d5
Charles de Freitas <charles@defreitas.io>
2020-07-03 15:43:59 +0100

    Update gitconfig and install

--- git/install.sh
+++ git/install.sh
@@ -4,6 +4,5 @@ SOURCE=$1
 
 echo "Setup git..."
 test -e "$SOURCE"/git/.gitconfig && mv "$HOME"/.gitconfig{,.bak}
-test -L "$SOURCE"/git/.gitconfig && mv "$HOME"/.gitconfig{,.bak}
-ln -s "$SOURCE"/git/.gitconfig "$HOME"/.gitconfig
+ln -sf "$SOURCE"/git/.gitconfig "$HOME"/.gitconfig
 
