93ba2ac064d9ce9c8832d2c6e279eec4bc81dfc0
Charles de Freitas <charles@defreitas.io>
2020-07-21 10:50:35 +0100

    Cleanup ignores and add global gitignore
    Rename git files

--- git/install.sh
+++ git/install.sh
@@ -3,6 +3,10 @@
 SOURCE=$1
 
 echo "Setup git..."
+echo "**** gitconfig ****"
 [ -e "$HOME"/.gitconfig ] && mv "$HOME"/.gitconfig{,.bak}
-ln -sfn "$SOURCE"/git/.gitconfig "$HOME"/.gitconfig
+ln -sfn "$SOURCE"/git/gitconfig "$HOME"/.gitconfig
 
+echo "**** gitignore ****"
+[ -e "$HOME"/.gitignore ] && mv "$HOME"/.gitignore{,.bak}
+ln -sfn "$SOURCE"/git/gitignore "$HOME"/.gitignore
