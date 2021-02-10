d97f686ae942f61d735f99a3358c25de6c22ef35
Charles de Freitas <me@chasbob.dev>
2021-02-10 11:30:40 +0000

    Update git config files

--- git/install.sh
+++ git/install.sh
@@ -3,10 +3,11 @@
 SOURCE=$1
 
 echo "Setup git..."
-echo "**** gitconfig ****"
-[ -e "$HOME"/.gitconfig ] && mv "$HOME"/.gitconfig{,.bak}
-ln -sfn "$SOURCE"/git/gitconfig "$HOME"/.gitconfig
-
-echo "**** gitignore ****"
-[ -e "$HOME"/.gitignore ] && mv "$HOME"/.gitignore{,.bak}
-ln -sfn "$SOURCE"/git/gitignore "$HOME"/.gitignore
+ln -sfn "$SOURCE"/git "$XDG_CONFIG_HOME"/git
+# echo "**** gitconfig ****"
+# [ -e "$HOME"/.gitconfig ] && mv "$HOME"/.gitconfig{,.bak}
+# ln -sfn "$SOURCE"/git/gitconfig "$HOME"/.gitconfig
+#
+# echo "**** gitignore ****"
+# [ -e "$HOME"/.gitignore ] && mv "$HOME"/.gitignore{,.bak}
+# ln -sfn "$SOURCE"/git/gitignore "$HOME"/.gitignore
